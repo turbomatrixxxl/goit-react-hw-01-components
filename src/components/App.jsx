64f7229b3b376a20c6c4c764b7974296d5bc9106
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2024-07-08 13:25:06 +0300

    updated ex 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,17 @@
+import Profile from './Profile';
+import user from '../Tema1-json/user.json';
+
 export const App = () => {
+  // console.log(user);
+  // console.log(user.username);
+  // console.log(user.tag);
+  // console.log(user.location);
+  // console.log(user.avatar);
+  // console.log(user.stats);
+  // console.log(user.stats.followers);
+  // console.log(user.stats.views);
+  // console.log(user.stats.likes);
+
   return (
     <div
       style={{
@@ -10,9 +23,13 @@ export const App = () => {
         color: '#010101',
       }}
     >
-      <p>React homework Radu</p>
-      <button>butt</button>
-      <span>abc</span>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
